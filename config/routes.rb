9b53482057dc981237cdbf973943fee4df792ec2
Lena Adam <lena.adam@me.com>
2020-08-11 15:28:26 +0200

    add task manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  delete "tasks/:id", to: "tasks#destroy"
 end
+
+# create route for new get
+# create route for create post
+# add new action to controller
+# add create action to controller
+# linkt to create
+# link to task
